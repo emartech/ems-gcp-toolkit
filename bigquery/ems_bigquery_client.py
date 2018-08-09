e1993aca434ed9f187d0d31c4b01199f52ae203f
jszule <janos.szule@emarsys.com>
2018-08-09 16:38:12 +0200

    Separate batch and sync query execution
    Ticket-id: DP-0064
    
    Co-authored-by: dsztanko <szt.dori@gmail.com>

--- bigquery/ems_bigquery_client.py
+++ bigquery/ems_bigquery_client.py
@@ -1,5 +1,7 @@
+from collections import Iterable
+
 from google.cloud import bigquery
-from google.cloud.bigquery import QueryJobConfig, QueryPriority
+from google.cloud.bigquery import QueryJobConfig, QueryPriority, QueryJob
 
 
 class EmsBigqueryClient:
@@ -7,7 +9,13 @@ class EmsBigqueryClient:
     def __init__(self, project_id: str):
         self.__bigquery_client = bigquery.Client(project_id)
 
-    def submit_query_job(self, query: str):
+    def submit_batch_query(self, query: str) -> str:
+        return self.__execute_query_job(query, QueryPriority.BATCH).job_id
+
+    def run_sync_query(self, query: str) -> Iterable:
+        return self.__execute_query_job(query, QueryPriority.INTERACTIVE).result()
+
+    def __execute_query_job(self, query: str, priority: QueryPriority) -> QueryJob:
         job_config = QueryJobConfig()
-        job_config.priority = QueryPriority.BATCH
+        job_config.priority = priority
         return self.__bigquery_client.query(query, job_config)
--- tests/bigquery/test_ems_bigquery_client.py
+++ tests/bigquery/test_ems_bigquery_client.py
@@ -1,13 +1,22 @@
+from collections import Iterable
 from unittest import TestCase
-from unittest.mock import patch, Mock, ANY
+from unittest.mock import patch, Mock
 
 from google.cloud import bigquery
 from google.cloud.bigquery import QueryJob, QueryPriority
+from google.cloud.bigquery.table import RowIterator
 
 from bigquery.ems_bigquery_client import EmsBigqueryClient
 
 
 class TestEmsBigqueryClient(TestCase):
+
+    def setUp(self):
+        self.test_query = "HELLO * BELLO"
+        self.client_mock = Mock()
+        self.query_job_mock = Mock(QueryJob)
+
+        self.client_mock.query.return_value = self.query_job_mock
 
     @patch("bigquery.ems_bigquery_client.bigquery")
     def test_init_clientCreatedInsideWithGivenProjectId(self, bigquery_module_patch: bigquery):
@@ -16,19 +25,29 @@ class TestEmsBigqueryClient(TestCase):
         bigquery_module_patch.Client.assert_called_once_with("some-project-id")
 
     @patch("bigquery.ems_bigquery_client.bigquery")
-    def test_submit_query_job_submitsABatchJobAndReturnsWithQueryJob(self, bigquery_module_patch: bigquery):
-        test_query = "HELLO * BELLO"
-        client_mock = Mock()
-        query_job_mock = Mock(QueryJob)
-
-        bigquery_module_patch.Client.return_value = client_mock
-        client_mock.query.return_value = query_job_mock
+    def test_submit_batch_query_submitsBatchQueryAndReturnsJobId(self, bigquery_module_patch: bigquery):
+        bigquery_module_patch.Client.return_value = self.client_mock
+        self.query_job_mock.job_id = "some-job-id"
 
         ems_bigquery_client = EmsBigqueryClient("some-project-id")
-        query_job = ems_bigquery_client.submit_query_job(test_query)
+        result_job_id = ems_bigquery_client.submit_batch_query(self.test_query)
 
-        client_mock.query.assert_called_once_with(test_query, ANY)
-        query_mock_result = client_mock.query.call_args_list[0][0][1]
+        query_mock_result = self.client_mock.query.call_args_list[0][0]
 
-        assert isinstance(query_job, QueryJob)
-        assert QueryPriority.BATCH == query_mock_result.priority
+        assert self.test_query == query_mock_result[0]
+        assert QueryPriority.BATCH == query_mock_result[1].priority
+        assert "some-job-id" == result_job_id
+
+    @patch("bigquery.ems_bigquery_client.bigquery")
+    def test_submit_sync_query_submitsInteractiveQueryAndReturnsWithResultIterator(self, bigquery_module_patch: bigquery):
+        bigquery_module_patch.Client.return_value = self.client_mock
+        self.query_job_mock.result.return_value = Mock(RowIterator)
+
+        ems_bigquery_client = EmsBigqueryClient("some-project-id")
+        result_rows = ems_bigquery_client.run_sync_query(self.test_query)
+
+        query_mock_result = self.client_mock.query.call_args_list[0][0]
+
+        assert self.test_query == query_mock_result[0]
+        assert QueryPriority.INTERACTIVE == query_mock_result[1].priority
+        assert isinstance(result_rows, Iterable)
