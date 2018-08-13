72f63528591178d94c6b4082c46f9a8f31776aea
Peter Horvath <peter.horvath@emarsys.com>
2018-08-13 13:33:11 +0200

    add simple setup.py
    Ticket-id: DP-0064
    
    Co-authored-by: dsztanko <szt.dori@gmail.com>

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name="ems-gcp-toolkit",
+    version="",
+    packages=["tests", "bigquery"],
+    url="",
+    license="",
+    author="ems",
+    author_email="",
+    description="",
+    install_requires=[
+        "google-cloud-bigquery"
+    ],
+)
