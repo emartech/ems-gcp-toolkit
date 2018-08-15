7867d99398f52c0b08ad31da0f82f1a9f038783d
Frantisek Csajka <fcsajka@emarsys.com>
2018-08-15 15:03:40 +0200

    Simple integration test
    Ticket-id: DP-0065
    
    Co-authored-by: dsztanko <szt.dori@gmail.com>

--- tests/integration/bigquery/it_ems_bigquery_client.py
+++ tests/integration/bigquery/it_ems_bigquery_client.py
@@ -1,7 +1,17 @@
 from unittest import TestCase
 
+from bigquery.ems_bigquery_client import EmsBigqueryClient
+
 
 class ItEmsBigqueryClient(TestCase):
 
-    def test_none(self):
-        assert True
+    def test_run_sync_query(self):
+        client = EmsBigqueryClient("ems-data-platform-dev")
+        result = client.run_sync_query("SELECT 1 AS data")
+
+        rows = list(result)
+        assert 1 == len(rows)
+        assert {"data": 1} == rows[0]
+
+# TODO
+# get project id from env
