925805d135c2a1c29659cc7e70401c68ebd217c9
Frantisek Csajka <fcsajka@emarsys.com>
2018-08-15 15:44:09 +0200

    Get project id from environment variable
    Ticket-id: DP-0065
    
    Co-authored-by: dsztanko <szt.dori@gmail.com>

--- tests/integration/bigquery/it_ems_bigquery_client.py
+++ tests/integration/bigquery/it_ems_bigquery_client.py
@@ -1,3 +1,4 @@
+import os
 from unittest import TestCase
 
 from bigquery.ems_bigquery_client import EmsBigqueryClient
@@ -6,12 +7,10 @@ from bigquery.ems_bigquery_client import EmsBigqueryClient
 class ItEmsBigqueryClient(TestCase):
 
     def test_run_sync_query(self):
-        client = EmsBigqueryClient("ems-data-platform-dev")
+        gcp_project_id = os.environ["GCP_PROJECT_ID"]
+        client = EmsBigqueryClient(gcp_project_id)
         result = client.run_sync_query("SELECT 1 AS data")
 
         rows = list(result)
         assert 1 == len(rows)
         assert {"data": 1} == rows[0]
-
-# TODO
-# get project id from env
