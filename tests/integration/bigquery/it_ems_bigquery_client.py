5adece5b06489cfb0c658c2358298685cf6b7d07
Frantisek Csajka <fcsajka@emarsys.com>
2018-08-16 15:23:55 +0200

    Integration test for async query execution
    Ticket-id: DP-0065
    
    Co-authored-by: dsztanko <szt.dori@gmail.com>

--- tests/integration/bigquery/it_ems_bigquery_client.py
+++ tests/integration/bigquery/it_ems_bigquery_client.py
@@ -1,24 +1,30 @@
 import os
 from unittest import TestCase
 
+from google.cloud import bigquery
+
 from bigquery.ems_api_error import EmsApiError
 from bigquery.ems_bigquery_client import EmsBigqueryClient
 
 
 class ItEmsBigqueryClient(TestCase):
-    GCP_PROJECT_ID = os.environ["GCP_PROJECT_ID"]
 
-    def test_run_sync_query(self):
-        client = EmsBigqueryClient(self.GCP_PROJECT_ID)
-        result = client.run_sync_query("SELECT 1 AS data")
+    GCP_PROJECT_ID = os.environ["GCP_PROJECT_ID"]
+    SIMPLE_QUERY = "SELECT 1 AS data"
+
+    def setUp(self):
+        self.client = EmsBigqueryClient(self.GCP_PROJECT_ID)
+
+    def test_run_sync_query_returnsValidValues(self):
+
+        result = self.client.run_sync_query(self.SIMPLE_QUERY)
 
         rows = list(result)
         assert 1 == len(rows)
         assert {"data": 1} == rows[0]
 
     def test_run_sync_query_non_existing_dataset(self):
-        client = EmsBigqueryClient(self.GCP_PROJECT_ID)
-        result = client.run_sync_query("SELECT * FROM `non_existing_dataset.whatever`")
+        result = self.client.run_sync_query("SELECT * FROM `non_existing_dataset.whatever`")
 
         with self.assertRaises(EmsApiError) as context:
             list(result)
@@ -27,3 +33,10 @@ class ItEmsBigqueryClient(TestCase):
         assert "not found" in error_message
         assert self.GCP_PROJECT_ID in error_message
         assert "non_existing_dataset" in error_message
+
+    def test_run_async_query_submitsJob(self):
+        job_id = self.client.run_async_query(self.SIMPLE_QUERY)
+
+        job = bigquery.Client(self.GCP_PROJECT_ID).get_job(job_id)
+
+        assert job.state is not None
