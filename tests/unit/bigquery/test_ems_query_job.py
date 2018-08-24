19cc3da480f60ceaf75a74a77c0bb969eb9cddb1
jszule <janos.szule@emarsys.com>
2018-08-24 10:28:08 +0200

    Add EmsQueryJob class
    Ticket-id: DP-0068
    
    Co-authored-by: feri <csferi27@gmail.com>

---
+++ tests/unit/bigquery/test_ems_query_job.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+from bigquery.ems_query_job import EmsQueryJob, EmsQueryState
+
+
+class TestEmsQueryJob(TestCase):
+    def setUp(self):
+        self.errors = [{"some": "error", "happened": "here"}]
+        self.ems_query_job = EmsQueryJob("test-job-id", EmsQueryState.DONE, self.errors)
+
+    def test_errors(self):
+        self.assertEquals(self.ems_query_job.errors, self.errors)
+
+    def test_state(self):
+        self.assertEquals(self.ems_query_job.state, EmsQueryState.DONE)
+
+    def test_job_id(self):
+        self.assertEqual(self.ems_query_job.job_id, "test-job-id")
+
+    def test_is_failed(self):
+        self.assertTrue(self.ems_query_job.is_failed)
+
+    def test_is_not_failed(self):
+        not_failed_ems_query_job = EmsQueryJob("test-job-id", EmsQueryState.DONE, None)
+
+        self.assertFalse(not_failed_ems_query_job.is_failed)
+
