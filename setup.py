e80dd8cd073aa49dc4151b5ceeb76db8a59b3147
Peter Horvath <peter.horvath@emarsys.com>
2018-08-29 11:09:46 +0200

    increase version to 0.0.3
    Ticket-id: DP-0068
    
    Co-authored-by: jszule <janos.szule@emarsys.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ems-gcp-toolkit",
-    version="0.0.2",
+    version="0.0.3",
     packages=["bigquery"],
     url="https://github.com/emartech/ems-gcp-toolkit",
     license="MIT",
