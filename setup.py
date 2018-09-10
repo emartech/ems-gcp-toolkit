d21423077e525ac47a0a7c3eb79dbf9a189019f1
Frantisek Csajka <fcsajka@emarsys.com>
2018-09-10 17:11:53 +0200

    Set new version
    Ticket-id: DP-0076
    
    Co-authored-by: bvarkoly <bvarkoly@emarsys.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ems-gcp-toolkit",
-    version="0.0.3",
+    version="0.1.0",
     packages=["bigquery"],
     url="https://github.com/emartech/ems-gcp-toolkit",
     license="MIT",
