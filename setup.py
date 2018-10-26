2665515df49d93c51314d36066cd7938d94b9cd0
Peter Horvath <peter.horvath@emarsys.com>
2018-10-26 21:50:04 +0200

    increase version
    Ticket-id: DP-0075

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ems-gcp-toolkit",
-    version="0.1.0",
+    version="0.1.1",
     packages=["bigquery"],
     url="https://github.com/emartech/ems-gcp-toolkit",
     license="MIT",
