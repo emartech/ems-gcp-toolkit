1788400bf4399b827f512aed64796be94b19a751
dsztanko <szt.dori@gmail.com>
2019-01-11 15:14:37 +0100

    Extend .gitignore and increase release number
    Ticket-id: DP-0132

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ems-gcp-toolkit",
-    version="0.1.4",
+    version="0.1.5",
     packages=["bigquery"],
     url="https://github.com/emartech/ems-gcp-toolkit",
     license="MIT",
