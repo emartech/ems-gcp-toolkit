b1aadf13f8a4e14ace86248e497376b2a5185531
abalogh2 <adam.balogh2@emarsys.com>
2019-01-16 11:38:38 +0100

    Add integration test for load jobs - happy path
    Ticket-id: DP-0132
    
    Co-authored-by: dsztanko <szt.dori@gmail.com>

--- tests/integration/bigquery/it_ems_bigquery_client.py
+++ tests/integration/bigquery/it_ems_bigquery_client.py
@@ -1,17 +1,22 @@
 import datetime
 import os
+import random
 import uuid
 from unittest import TestCase
 
 from google.api_core.exceptions import NotFound
 from google.cloud import bigquery
+from google.cloud import storage
 from google.cloud.bigquery import Dataset, DatasetReference, Table, TableReference, SchemaField, TimePartitioning
 from google.cloud.exceptions import Conflict
-from tenacity import retry, stop_after_delay
+from tenacity import retry, stop_after_delay, retry_if_result
 
 from bigquery.ems_api_error import EmsApiError
 from bigquery.ems_bigquery_client import EmsBigqueryClient
+from bigquery.job.config.ems_job_config import EmsWriteDisposition
+from bigquery.job.config.ems_load_job_config import EmsLoadJobConfig
 from bigquery.job.config.ems_query_job_config import EmsQueryJobConfig
+from bigquery.job.ems_job_state import EmsJobState
 
 
 class ItEmsBigqueryClient(TestCase):
@@ -146,9 +151,48 @@ class ItEmsBigqueryClient(TestCase):
         table_reference = TableReference(self.DATASET, self.table_reference.table_id + "_copy")
         self.GCP_BIGQUERY_CLIENT.copy_table(sources=self.table_reference, destination=table_reference)
 
+    def test_run_async_load_job_loadsFileFromBucketToNewBigqueryTable(self):
+        storage_client = storage.Client(self.GCP_PROJECT_ID,)
+        bucket_name = "it_test_ems_gcp_toolkit"
+        try:
+            bucket = storage_client.get_bucket(bucket_name)
+        except NotFound:
+            bucket = storage_client.bucket(bucket_name)
+            bucket.location = "europe-west1"
+            bucket.storage_class = "REGIONAL"
+            bucket.create()
+        blob_name = "sample_fruit_test.csv"
+        blob = bucket.blob(blob_name)
+        random_quantity = random.randint(10000, 99000)
+        blob.upload_from_string(f"apple,{random_quantity},True,1970-01-01T12:00:00.000Z\n")
+        config = EmsLoadJobConfig(destination_project_id=self.GCP_PROJECT_ID,
+                                  destination_dataset=self.DATASET.dataset_id,
+                                  destination_table="load_job_test",
+                                  schema={"fields": [{"type": "STRING", "name": "fruit"},
+                                                     {"type": "INT64", "name": "quantity"},
+                                                     {"type": "BOOL", "name": "is_delicious"},
+                                                     {"type": "TIMESTAMP", "name": "best_before"}]},
+                                  write_disposition=EmsWriteDisposition.WRITE_TRUNCATE)
+
+        load_job_id = self.client.run_async_load_job(f"gs://{bucket_name}/{blob_name}", "it_test", config)
+        self.__wait_for_job_done(load_job_id)
+
+        query = f"""
+        SELECT * from `{config.destination_project_id}.{config.destination_dataset}.{config.destination_table}`
+        """
+
+        result = self.client.run_sync_query(query=query)
+        expected = [{"fruit": "apple", "quantity": random_quantity, "is_delicious": True,
+                 "best_before": datetime.datetime(1970, 1, 1, 12, 0, 0, tzinfo=datetime.timezone.utc)}]
+        self.assertEquals(expected, list(result))
+
     @retry(stop=(stop_after_delay(10)))
     def __wait_for_job_submitted(self, job_id):
-        self.GCP_BIGQUERY_CLIENT.get_job(job_id).state is not None
+        self.GCP_BIGQUERY_CLIENT.get_job(job_id)
+
+    @retry(stop=(stop_after_delay(10)), retry=(retry_if_result(lambda result: result != EmsJobState.DONE.value)))
+    def __wait_for_job_done(self, job_id):
+        return self.GCP_BIGQUERY_CLIENT.get_job(job_id).state
 
     def __get_table_path(self):
         return "{}.{}.{}".format(ItEmsBigqueryClient.GCP_PROJECT_ID, ItEmsBigqueryClient.DATASET.dataset_id,
