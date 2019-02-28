4964c74bdf72ca0a4394447c0da52d0eed25ca5c
kkimak <krisztian.kimak@emarsys.com>
2019-02-28 10:54:38 +0100

    created publisher client subscription create
    Ticket-id: CDP-027
    
    Co-authored-by: erik <erko256@gmail.com>

--- pubsub/ems_publisher_client.py
+++ pubsub/ems_publisher_client.py
@@ -1,6 +1,6 @@
 from concurrent.futures import Future
 
-from google.cloud.pubsub_v1 import PublisherClient
+from google.cloud.pubsub_v1 import PublisherClient, SubscriberClient
 
 
 class EmsPublisherClient:
@@ -11,3 +11,11 @@ class EmsPublisherClient:
 
     def topic_create(self, project_id: str, topic_name: str):
         self.__client.api.create_topic(self.__client.api.topic_path(project_id, topic_name))
+
+    def subscription_create(self, project_id: str, topic_name: str, subscription_name: str):
+        subscriber = SubscriberClient()
+
+        topic_path = subscriber.topic_path(project_id, topic_name)
+        subscription_path = subscriber.subscription_path(project_id, subscription_name)
+
+        subscriber.create_subscription(subscription_path, topic_path)
--- tests/integration/pubsub/it_ems_publisher_client.py
+++ tests/integration/pubsub/it_ems_publisher_client.py
@@ -15,7 +15,7 @@ class ItEmsPublisherClient(TestCase):
 
     def test_topic_create(self):
         publisher = PublisherClient()
-        expected_topic_name = "test_topic" + str(int(time.time()))
+        expected_topic_name = self.generate_test_name('topic')
         expected_topic_path = publisher.api.topic_path(GCP_PROJECT_ID, expected_topic_name)
 
         self.ems_client.topic_create(GCP_PROJECT_ID, expected_topic_name)
@@ -26,3 +26,31 @@ class ItEmsPublisherClient(TestCase):
             self.fail(f"Topic not created with name {expected_topic_name}")
 
         self.assertEqual(topic.name, expected_topic_path)
+
+    def test_subscription_create(self):
+        publisher = PublisherClient()
+        expected_topic_name = self.generate_test_name('topic')
+        expected_subscription_name = self.generate_test_name('subscription')
+        expected_subscription_list = ['projects/' + GCP_PROJECT_ID + '/subscriptions/' + expected_subscription_name]
+        subscription_list = []
+
+        self.ems_client.topic_create(GCP_PROJECT_ID, expected_topic_name)
+        self.ems_client.subscription_create(GCP_PROJECT_ID, expected_topic_name, expected_subscription_name)
+
+        try:
+            topic_path = publisher.topic_path(GCP_PROJECT_ID, expected_topic_name)
+            subscriptions = publisher.list_topic_subscriptions(topic_path)
+
+            for subscription in subscriptions:
+                subscription_list.append(subscription)
+        except NotFound:
+            self.fail(
+                f"""Subscription not created with topic name {expected_topic_name}, 
+                subscription name {expected_subscription_name}""")
+
+        self.assertNotEqual(len(subscription_list), 0, 'Subscription list is empty')
+        self.assertEquals(subscription_list, expected_subscription_list, 'Subscriptions not created')
+
+    @staticmethod
+    def generate_test_name(context: str):
+        return "test_" + context + "_" + str(int(time.time()))
