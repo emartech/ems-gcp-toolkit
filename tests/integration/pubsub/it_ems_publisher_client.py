0ec1ab6671258e7fce3389e7fb5ecc0dd55c7247
Gabor Barnak <gabor.barnak@gmail.com>
2019-03-01 13:09:50 +0100

    Publisher client refactor
    Ticket-id: CDP-027
    
    Co-authored-by: abalogh2 <adam.balogh2@emarsys.com>

--- tests/integration/pubsub/it_ems_publisher_client.py
+++ tests/integration/pubsub/it_ems_publisher_client.py
@@ -15,7 +15,7 @@ class ItEmsPublisherClient(TestCase):
         self.publisher = PublisherClient()
 
     def test_topic_create_if_not_exists_new_topic_creation(self):
-        expected_topic_name = self.generate_test_name("topic")
+        expected_topic_name = self.__generate_test_name("topic")
         expected_topic_path = self.publisher.api.topic_path(GCP_PROJECT_ID, expected_topic_name)
 
         self.ems_client.topic_create_if_not_exists(GCP_PROJECT_ID, expected_topic_name)
@@ -27,10 +27,10 @@ class ItEmsPublisherClient(TestCase):
 
         self.assertEqual(topic.name, expected_topic_path)
 
-        self.delete_topic(expected_topic_name)
+        self.__delete_topic(expected_topic_name)
 
     def test_topic_create_if_not_exists_topic_already_created(self):
-        expected_topic_name = self.generate_test_name("topic")
+        expected_topic_name = self.__generate_test_name("topic")
         self.publisher.api.topic_path(GCP_PROJECT_ID, expected_topic_name)
 
         self.ems_client.topic_create_if_not_exists(GCP_PROJECT_ID, expected_topic_name)
@@ -40,11 +40,11 @@ class ItEmsPublisherClient(TestCase):
         except AlreadyExists:
             self.fail(f"Topic already exists but tried to recreate with name {expected_topic_name}")
 
-        self.delete_topic(expected_topic_name)
+        self.__delete_topic(expected_topic_name)
 
     def test_subscription_create(self):
-        expected_topic_name = self.generate_test_name("topic")
-        expected_subscription_name = self.generate_test_name("subscription")
+        expected_topic_name = self.__generate_test_name("topic")
+        expected_subscription_name = self.__generate_test_name("subscription")
         expected_subscription_list = ["projects/" + GCP_PROJECT_ID + "/subscriptions/" + expected_subscription_name]
         subscription_list = []
 
@@ -66,18 +66,18 @@ class ItEmsPublisherClient(TestCase):
         self.assertNotEqual(len(subscription_list), 0, "Subscription list is empty")
         self.assertEqual(subscription_list, expected_subscription_list, "Subscriptions not created")
 
-        self.delete_topic(expected_topic_name)
-        self.delete_subscription(expected_subscription_name)
+        self.__delete_subscription(expected_subscription_name)
+        self.__delete_topic(expected_topic_name)
 
-    def delete_topic(self, expected_topic_name):
+    def __delete_topic(self, expected_topic_name):
         self.publisher.api.delete_topic(self.publisher.api.topic_path(GCP_PROJECT_ID, expected_topic_name))
 
     @staticmethod
-    def delete_subscription(subscription_name: str):
+    def __delete_subscription(subscription_name: str):
         subscriber = SubscriberClient()
         subscription_path = subscriber.api.subscription_path(GCP_PROJECT_ID, subscription_name)
         subscriber.api.delete_subscription(subscription_path)
 
     @staticmethod
-    def generate_test_name(context: str):
+    def __generate_test_name(context: str):
         return "test_" + context + "_" + str(int(time.time()))
