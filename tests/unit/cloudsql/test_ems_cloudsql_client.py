6e904134bca1c2e1abc7b78a557665db0b1da421
dsztanko <szt.dori@gmail.com>
2019-03-18 14:26:25 +0100

    Patch discovery module in unit test
    Ticket-id: CDP-036
    
    Co-authored-by: feri <csferi27@gmail.com>

--- tests/unit/cloudsql/test_ems_cloudsql_client.py
+++ tests/unit/cloudsql/test_ems_cloudsql_client.py
@@ -1,10 +1,12 @@
 from unittest import TestCase
+from unittest.mock import patch
 
 from cloudsql.ems_cloudsql_client import EmsCloudsqlClient
 
 
 class TestEmsCloudsqlClient(TestCase):
 
+    @patch("cloudsql.ems_cloudsql_client.discovery")
     def test_properties(self):
         client = EmsCloudsqlClient("some-project-id", "some-instance-id")
 
