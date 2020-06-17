8e7bb9241456de4d78ce2ad39fa6119c0ca0e1ba
Peter Horvath <peter.horvath@emarsys.com>
2020-06-17 16:58:46 +0200

    refactor import
    Ticket-id: CDP-986
    
    Co-authored-by: Feri <csferi27@gmail.com>

--- spanner/ems_spanner_client.py
+++ spanner/ems_spanner_client.py
@@ -1,10 +1,10 @@
-from google.cloud import spanner
+from google.cloud.spanner import Client
 
 
 class EmsSpannerClient:
     def __init__(self, project_id: str, instance_id, db_id):
         self.project_id = project_id
-        self.__db = spanner.Client(project_id).instance(instance_id).database(db_id)
+        self.__db = Client(project_id).instance(instance_id).database(db_id)
 
     def execute_sql(self, query: str):
         with self.__db.snapshot() as snapshot:
